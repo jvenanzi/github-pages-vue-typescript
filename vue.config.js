98fc6a2f251122db62136425c7d13e0654408c63
Joseph Venanzio <jvenanzi@nd.edu>
2022-12-07 10:31:55 -0500

    commented out path in vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'/github-pages-vue-typescript/'
+  // publicPath:'/github-pages-vue-typescript/'
 })
